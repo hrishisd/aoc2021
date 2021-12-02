d40942a45bb9ed4f704877bf5fa1d94dd6d9de56
hrishisd <hdharam@berkeley.edu>
2021-12-02 11:51:14 -0500

    add day 2 python solution

---
+++ 02/python/main.py
@@ -0,0 +1,37 @@
+from typing import Tuple
+from functools import reduce
+
+def part1(steps):
+    def sum_pairs(p1, p2):
+        return (p1[0] + p2[0], p1[1] + p2[1])
+    x,y = reduce(sum_pairs, steps)
+    return x * y
+
+def part2(steps):
+    aim, x_pos, depth = 0, 0, 0 
+    for fwd,aim_delta in steps:
+        if aim_delta:
+            aim += aim_delta
+        else:
+            x_pos += fwd
+            depth += aim * fwd
+    return x_pos * depth
+
+
+with open("../input.txt") as f:
+    def parse_step(line) -> Tuple[int, int]:
+        direction, magnitude = line.split()
+        magnitude = int(magnitude)
+        if direction == "up":
+            return 0, -magnitude
+        elif direction == "down":
+            return 0, magnitude
+        elif direction == "forward":
+            return magnitude, 0
+        else:
+            raise ValueError()
+    
+    steps = [parse_step(line.strip()) for line in f]
+    print(part1(steps))
+    print(part2(steps))
+        
